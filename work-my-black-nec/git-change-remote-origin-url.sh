e5309322c7abe8d88e6bfd6eaae62b545b354322
alaa 2 amz <alaa2amz@gmail.com>
2020-01-19 10:36:02 +0200

    Sun Jan 19 10:36:02 EET 2020 datep ghddsfdsfdgd

--- work-my-black-nec/git-change-remote-origin-url.sh
+++ work-my-black-nec/git-change-remote-origin-url.sh
@@ -1,1 +1,10 @@
 git remote set-url origin "$1"
+#change the origin url to "https://username:password@github.com".
+
+
+
+cat >>~/.netrc <<EOF
+machine some_git_server.com
+       login alaa2amz
+       password qgjkrrr69-GH
+EOF
