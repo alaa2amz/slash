0e6646ec85d68e17576ae61aff4d36651f3f81ac
alaa2amz <alaa2amz@gmail.com>
2020-01-19 08:17:06 +0200

    adding-gacp

---
+++ work-my-black-nec/git_add_commit_push.sh
@@ -0,0 +1,7 @@
+#cp to root folder of repository
+#save current directory
+#change to root repository
+#git add all
+#git commit time stamp + rando word + files summary + comment argument supplied if found
+#git push
+#cd back to current directory
