38af5917e6ffae1db93edb4fbbf663e77ae0ac4f
alaa 2 amz <alaa2amz@gmail.com>
2020-01-19 10:04:45 +0200

    Sun Jan 19 10:04:45 EET 2020 rr ghdgdg

--- work-my-black-nec/git_add_commit_push.sh
+++ work-my-black-nec/git_add_commit_push.sh
@@ -5,3 +5,12 @@
 #git commit time stamp + rando word + files summary + comment argument supplied if found
 #git push
 #cd back to current directory
+GIT_REPO_PATH="$HOME/slash/work-my-black-nec"
+CURRENT_DIR_PATH="$PWD"
+cp -bv "$1" "${GIT_REPO_PATH}/$1"
+cd $GIT_REPO_PATH
+git add .
+git commit -a -m "`/bin/date` $1 $2"
+git push origin master
+alaa2amz
+cd $CURRENT_DIR_PATH
