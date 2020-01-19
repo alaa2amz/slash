79f60b4d33d5027018e195c1590f10bfe5d85bd9
alaa 2 amz <alaa2amz@gmail.com>
2020-01-19 10:42:46 +0200

    Sun Jan 19 10:42:46 EET 2020 datep ghddsfdsfdgd

--- work-my-black-nec/git_add_commit_push.sh
+++ work-my-black-nec/git_add_commit_push.sh
@@ -5,6 +5,7 @@
 #git commit time stamp + rando word + files summary + comment argument supplied if found
 #git push
 #cd back to current directory
+GIT_ASKPASS=qgjkrrr69-GH
 GIT_REPO_PATH="$HOME/slash/work-my-black-nec"
 CURRENT_DIR_PATH="$PWD"
 cp -bv "$1" "${GIT_REPO_PATH}/$1"
